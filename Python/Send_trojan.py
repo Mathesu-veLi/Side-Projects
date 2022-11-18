4df4c858e4c04719ccf27bd06c12d0c214e6e3d3
Matheus Levi <matheuslevit@gmail.com>
2022-11-18 15:37:50 -0300

    Mudança do Custom Tkinter para o Tkinter e adição de popups
    Troquei o Custom Tkinter pelo Tkinter, por conta de erros ao tentar transformar em .exe em --onefile, só conseguindo transformar o mesmo em --onedir
    Adicionei popups com erros por conta de ser mais fácil do que a manipulação de local de cada Label error para esconder o erro passado

--- Python/Send_trojan.py
+++ Python/Send_trojan.py
@@ -1,11 +1,19 @@
-from customtkinter import *
+from tkinter import *
+import tkinter.ttk as tkk
+import tkinter.messagebox
 import pyautogui, keyboard
 
+
+bC = '#3f3f3f'
+
+
+
 def flood():
+    
     if msg.get() != '' and vezes.get() != '' and vezes.get != '0':
         while True:
-            f2 = CTkLabel(master= janela, text='                    Aperte F2 para começar a flodar!            \n  ', text_color='green')
-            f2.place(x=20, y=125)
+            f2 = Label(master= janela, text='Aperte F2 para começar a flodar!', fg='green', bg=bC)
+            f2.pack(side=BOTTOM)
             if keyboard.is_pressed('f2'):
                 for c in range(0, int(vezes.get()) + 1):
                     pyautogui.write(f'{msg.get()} {c}')
@@ -14,39 +22,32 @@ def flood():
                         break
                 break
     else:
-        if msg.get() == '':
-            try:
-                error.destroy()
-            except:
-                error = CTkLabel(master=janela, text='Digite uma mensagem para começar a enviar!     \n ', text_color='red')
-                error.place(x=30, y=125)
-        if vezes.get() == '':
-            try:
-                error.destroy()
-            except:
-                error = CTkLabel(master=janela, text='Digite o número de vezes que a mensagem será\nenviada para começar a envia-las!', text_color='red')
-                error.place(x=30, y=125)
+        if msg.get() == '' or msg.get() == None:
+            tkinter.messagebox.showerror('Error!', 'Digite uma mensagem para começar a enviar!')
+        if vezes.get() == '' or vezes.get() == None:
+            tkinter.messagebox.showerror('Error!', 'Digite o número de vezes que a mensagem será enviada para começar a envia-las!')
         elif vezes.get() == '0':
-            try:
-                error.destroy()
-            except:
-                error = CTkLabel(master=janela, text='Não posso enviar um número 0 de vezes!', text_color='red')
-                error.place(x=30, y=125)
+            tkinter.messagebox.showerror('Error!', 'Não posso enviar um número 0 de vezes!')
 
-janela = CTk()
+janela = Tk()
+janela.configure(background=bC)
 janela.geometry("330x190")
 janela.title('Send Trojan')
-set_default_color_theme("dark-blue") 
-textoDeOrientacao = CTkLabel(janela, text='Envie 1 mensagem varias vezes!')
-textoDeOrientacao.pack(side=TOP)
-msgLabel = CTkLabel(janela, text='Mensagem a ser enviada:')
+janela.resizable(width=False, height=False)
+style = tkk.Style()
+style.configure("Label", foreground="white", background=bC)
+textoDeOrientacao = tkk.Label(janela, text='Envie 1 mensagem varias vezes!', style="Label")
+textoDeOrientacao.pack(side=TOP, pady=5)
+msgLabel = tkk.Label(janela, text='Mensagem a ser enviada:', style="Label")
 msgLabel.place(x=5, y=50)
-msg = CTkEntry(master=janela)
+msg = Entry(master=janela, bg='#2f2f2f', fg='white')
 msg.place(x=180, y=50)
-vezesLabel = CTkLabel(janela, text='Vezes que ela será enviada:')
+msg.config(highlightbackground=bC)
+vezesLabel = tkk.Label(janela, text='Vezes que ela será enviada:', style="Label")
 vezesLabel.place(x=5, y=90)
-vezes = CTkEntry(janela)
+vezes = Entry(janela, bg='#2f2f2f', fg='white')
 vezes.place(x=180, y=90)
-confirmButtom = CTkButton(janela, text='Começar a enviar!', command=flood)
+vezes.config(highlightbackground=bC)
+confirmButtom = Button(janela, text='Começar a enviar!', bg='blue', fg='white', activebackground='#2f2f2f', activeforeground='#7FFFD4' , command=flood, borderwidth=5)
 confirmButtom.pack(side=BOTTOM, pady=5)
 janela.mainloop()
