db4817649064289c0bbdfdf1cd5121ac59a4231b
Matheus Levi <matheuslevit@gmail.com>
2023-07-10 18:57:57 -0300

    Adicionando mensagem de sucesso

--- JS/Projeto Agenda/src/controllers/loginController.js
+++ JS/Projeto Agenda/src/controllers/loginController.js
@@ -24,3 +24,27 @@ exports.register = async function (req, res) {
         res.render('404')
     };
 };
+
+exports.login = async function (req, res) {
+    try {
+        const login = new Login(req.body);
+        await login.login();
+
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(function() {
+                return res.redirect('/login');
+            });
+            return;
+        };
+
+        req.flash('success', 'Você logou com sucesso!');
+        req.session.user = login.user;
+        req.session.save(function () {
+            return res.redirect('/login');
+        });
+    } catch (e) {
+        console.log(e)
+        return res.render('404')
+    };
+}
