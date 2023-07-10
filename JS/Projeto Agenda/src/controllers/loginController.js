963db4c0316703c9678385b284af61f09112dd70
Matheus Levi <matheuslevit@gmail.com>
2023-07-10 15:54:33 -0300

    Criando flash menssage com erros

--- JS/Projeto Agenda/src/controllers/loginController.js
+++ JS/Projeto Agenda/src/controllers/loginController.js
@@ -4,8 +4,16 @@ exports.index = (req, res) => {
     res.render('login');
 };
 
-exports.register = (req, res) => {
+exports.register = async function(req, res) {
     const login = new Login(req.body);
-    login.register();
+    await login.register();
+
+    if(login.errors.length > 0) {
+        req.flash('errors', login.errors);
+        req.session.save(function() {
+            return res.redirect('/login');
+        });
+        return;
+    };
     res.send(login.errors);
 }
