5ed9f4f66d8d15aefca1b3f9eae0efc8f17959e7
Matheus Levi <matheuslevit@gmail.com>
2023-07-10 15:58:52 -0300

    Envolvendo em bloco try catch

--- JS/Projeto Agenda/src/controllers/loginController.js
+++ JS/Projeto Agenda/src/controllers/loginController.js
@@ -4,16 +4,19 @@ exports.index = (req, res) => {
     res.render('login');
 };
 
-exports.register = async function(req, res) {
-    const login = new Login(req.body);
-    await login.register();
+exports.register = async function (req, res) {
+    try {
+        const login = new Login(req.body);
+        await login.register();
 
-    if(login.errors.length > 0) {
-        req.flash('errors', login.errors);
-        req.session.save(function() {
-            return res.redirect('/login');
-        });
-        return;
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(function () {
+                return res.redirect('/login');
+            });
+            return;
+        };
+    } catch (e) {
+        console.log(e)
     };
-    res.send(login.errors);
-}
+};
