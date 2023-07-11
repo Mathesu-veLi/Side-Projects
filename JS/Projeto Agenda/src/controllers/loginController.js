e7a28b407da6cb6ddbaccf838a135d01b3c5b74d
Matheus Levi <matheuslevit@gmail.com>
2023-07-10 22:02:06 -0300

    Ajustando o código visualmente

--- JS/Projeto Agenda/src/controllers/loginController.js
+++ JS/Projeto Agenda/src/controllers/loginController.js
@@ -29,22 +29,22 @@ exports.login = async function (req, res) {
     try {
         const login = new Login(req.body);
         await login.login();
-
-        if (login.errors.length > 0) {
-            req.flash('errors', login.errors);
-            req.session.save(function() {
-                return res.redirect('/login');
-            });
-            return;
-        };
-
-        req.flash('success', 'Você logou com sucesso!');
-        req.session.user = login.user;
-        req.session.save(function () {
+    
+        if(login.errors.length > 0) {
+          req.flash('errors', login.errors);
+          req.session.save(function() {
             return res.redirect('/login');
+          });
+          return;
+        }
+    
+        req.flash('success', 'Você entrou no sistema.');
+        req.session.user = login.user;
+        req.session.save(function() {
+          return res.redirect('/login');
         });
-    } catch (e) {
-        console.log(e)
-        return res.render('404')
-    };
+      } catch(e) {
+        console.log(e);
+        return res.render('404');
+      }
 }
