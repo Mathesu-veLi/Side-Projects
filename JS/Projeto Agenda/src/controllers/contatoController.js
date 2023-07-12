60eae4ff68bbbf6b3c6d62b1b2fc8475f00dd30b
Matheus Levi <matheuslevit@gmail.com>
2023-07-12 20:00:29 -0300

    Editando contato

--- JS/Projeto Agenda/src/controllers/contatoController.js
+++ JS/Projeto Agenda/src/controllers/contatoController.js
@@ -1,3 +1,4 @@
+const { async } = require('regenerator-runtime');
 const Contato = require('../models/ContatoModel');
 
 exports.index = (req, res) => {
@@ -10,6 +11,7 @@ exports.register = async (req, res) => {
     try {
         const contato = new Contato(req.body);
         await contato.register();
+
         if (contato.errors.length > 0) {
             req.flash('errors', contato.errors);
             req.session.save(() => res.redirect('/contato'));
@@ -26,11 +28,32 @@ exports.register = async (req, res) => {
 };
 
 
-exports.editIndex = async function(req, res) {
-    if(!req.params.id) return res.render('404');
+exports.editIndex = async function (req, res) {
+    if (!req.params.id) return res.render('404');
 
     const contato = await Contato.buscaPorId(req.params.id);
-    if(!contato) return res.render('404');
+    if (!contato) return res.render('404');
 
     res.render('contato', { contato });
 };
+
+exports.edit = async function (req, res) {
+    try {
+        if (!req.params.id) return res.render('404');
+        const contato = new Contato(req.body);
+        await contato.edit(req.params.id);
+
+        if (contato.errors.length > 0) {
+            req.flash('errors', contato.errors);
+            req.session.save(() => res.redirect('/contato'));
+            return;
+        };
+
+        req.flash('success', 'Contato editado com sucesso');
+        req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
+        return;
+    } catch (e) {
+        console.log(e);
+        res.render('404');
+    }
+}
