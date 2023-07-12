3e6f7838fa4ec25d9406ecf6eaf883f39243c0d4
Matheus Levi <matheuslevit@gmail.com>
2023-07-12 15:50:57 -0300

    Registrando contato

--- JS/Projeto Agenda/src/controllers/contatoController.js
+++ JS/Projeto Agenda/src/controllers/contatoController.js
@@ -1,3 +1,30 @@
+const Contato = require('../models/ContatoModel');
+
 exports.index = (req, res) => {
+    res.render('contato');
+};
+
+exports.register = async (req, res) => {
+    try {
+        const contato = new Contato(req.body);
+        await contato.register();
+        if (contato.errors.length > 0) {
+            req.flash('errors', contato.errors);
+            req.session.save(() => res.redirect('/contato'));
+            return
+        };
+
+        req.flash('success', 'Contato registrado com sucesso');
+        req.session.save(() => res.redirect(`/index/${contato.contato._id}`));
+        return;
+    } catch (e) {
+        console.log(e);
+        return res.render('404');
+    };
+};
+
+
+ports.editIndex = function(req, res) {
+    if(!req.params.id) return res.render('404');
     res.render('contato');
 };
