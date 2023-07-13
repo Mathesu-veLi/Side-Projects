a25f76452e425b30366b7e0e64384dae1da2a01b
Matheus Levi <matheuslevit@gmail.com>
2023-07-12 21:11:50 -0300

    Enviando email do usuario logado para o ContatoModel

--- JS/Projeto Agenda/src/controllers/homeController.js
+++ JS/Projeto Agenda/src/controllers/homeController.js
@@ -1,4 +1,11 @@
-exports.index = (req, res) => {
-    res.render('index')
-    return;
+const Contato = require('../models/ContatoModel');
+
+exports.index = async (req, res) => {
+    try {
+        contatos = await Contato.buscaContatos(req.session.user.email);
+    } catch (e) {
+        contatos = await Contato.buscaContatos('');
+    }
+    
+    res.render('index', { contatos });
 };
