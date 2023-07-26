a020f3dcb895629311eeb35b2c00752a81afe286
Matheus Levi <matheuslevit@gmail.com>
2023-07-26 18:58:55 -0300

    Validando nome

--- JS/Api Rest/src/models/User.js
+++ JS/Api Rest/src/models/User.js
@@ -1,11 +1,23 @@
 import Sequelize, { Model } from 'sequelize';
+import { defaultValueSchemable } from 'sequelize/types/utils';
 
 export default class User extends Model {
   static init(sequelize) {
     super.init({
-      nome: Sequelize.STRING,
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'Campo nome deve ter entre 3 e 255 caracteres'
+          },
+
+        }
+      },
       email: Sequelize.STRING,
-      password_hash: Sequelize.STRING
+      password_hash: Sequelize.STRING,
+      password: Sequelize.VIRTUAL,
     }, {
       sequelize,
     });
