f1c61ecf6f1cfcb7106ca5ebecae2d72caa91ebb
Matheus Levi <matheuslevit@gmail.com>
2023-07-26 19:36:11 -0300

    Recebendo requisição

--- JS/Api Rest/src/controllers/UserController.js
+++ JS/Api Rest/src/controllers/UserController.js
@@ -3,14 +3,12 @@ import User from '../models/User';
 class UserController {
   async store(req, res) {
     try {
-      const novoUser = await User.create({
-        nome: 'Matheus',
-        email: 'matheuslevit@gmail.com',
-        password: 'veli15'
-      })
+      const novoUser = await User.create(req.body);
       res.json(novoUser);
-    } catch(e) {
-      res.status(400).json(e.errors.map(erro => erro.message))
+    } catch (e) {
+      res.status(400).json({
+        errors: e.errors.map(erro => erro.message),
+      })
     };
   };
 };
