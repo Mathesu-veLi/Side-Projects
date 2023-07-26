8effa1e4d979bf5bc5deb90a315d702713e98949
Matheus Levi <matheuslevit@gmail.com>
2023-07-26 19:24:14 -0300

    Tratando erros

--- JS/Api Rest/src/controllers/UserController.js
+++ JS/Api Rest/src/controllers/UserController.js
@@ -2,12 +2,16 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    const novoUser = await User.create({
-      nome: 'Matheus',
-      email: 'matheuslevit@gmail.com',
-      password: 'vel'
-    })
-    res.json(novoUser);
+    try {
+      const novoUser = await User.create({
+        nome: 'Matheus',
+        email: 'matheuslevit@gmail.com',
+        password: 'veli15'
+      })
+      res.json(novoUser);
+    } catch(e) {
+      res.status(400).json(e.errors.map(erro => erro.message))
+    };
   };
 };
 
