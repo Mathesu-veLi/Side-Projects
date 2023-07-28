df2b046c9e5fe2dcf34a7995791c13c350672a42
Matheus Levi <matheuslevit@gmail.com>
2023-07-28 15:46:00 -0300

    Mostrando usuário específico de acordo com seu id

--- JS/Api Rest/src/controllers/UserController.js
+++ JS/Api Rest/src/controllers/UserController.js
@@ -20,6 +20,22 @@ class UserController {
       return res.json(null);
     }
   }
+
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      if (!user) {
+        return res.status(400).json({
+          errors: ['ID inexistente']
+        })
+      }
+      return res.json(user)
+    } catch(e) {
+      return res.status(400).json({
+        errors: e.errors.map(erro => erro.message),
+      })
+    }
+  }
 };
 
 export default new UserController();
