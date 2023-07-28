5f7b3cf3049fe1878cc2bf07c8a9584a27b72a13
Matheus Levi <matheuslevit@gmail.com>
2023-07-28 20:47:53 -0300

    Criando função show

--- JS/Api Rest/src/controllers/AlunoController.js
+++ JS/Api Rest/src/controllers/AlunoController.js
@@ -5,6 +5,37 @@ class AlunoController {
     const alunos = await Aluno.findAll()
     res.json(alunos);
   };
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      if(!id) {
+        return res.status(400).json({
+          errors: ['Faltando ID'],
+        });
+      };
+
+      const aluno = await Aluno.findByPk(id);
+
+      if(!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        });
+      };
+
+      return res.json(aluno);
+    } catch(e) {
+      return res.status(400).json({
+        errors: e.errors.map(err => err.message)
+      });
+    };
+  };
+
+  async store(req, res) {};
+
+  async update(req, res) {};
+
+  async delete(req, res) {};
 };
 
 export default new AlunoController();
