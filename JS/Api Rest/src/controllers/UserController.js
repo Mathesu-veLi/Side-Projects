9fb3408cdcc9cc6327e1a78570033094c3eb7c03
Matheus Levi <matheuslevit@gmail.com>
2023-07-28 18:55:30 -0300

    Utilizando o id do usuário logado

--- JS/Api Rest/src/controllers/UserController.js
+++ JS/Api Rest/src/controllers/UserController.js
@@ -14,7 +14,7 @@ class UserController {
 
   async index(req, res) {
     try {
-      const users = await User.findAll();
+      const users = await User.findAll({ attributes: ['id', 'nome', 'email'] });
       return res.json(users);
     } catch (e) {
       return res.json(null);
@@ -27,9 +27,11 @@ class UserController {
       if (!user) {
         return res.status(400).json({
           errors: ['ID inexistente']
-        })
-      }
-      return res.json(user)
+        });
+      };
+
+      const {id, nome, email} = user
+      return res.json({id, nome, email})
     } catch (e) {
       return res.status(400).json({
         errors: e.errors.map(erro => erro.message),
@@ -39,13 +41,7 @@ class UserController {
 
   async update(req, res) {
     try {
-      if (!req.params.id) {
-        return res.status(400).json({
-          errors: ['ID não enviado']
-        })
-      }
-
-      const user = await User.findByPk(req.params.id);
+      const user = await User.findByPk(req.userId);
       if (!user) {
         return res.status(400).json({
           errors: ['Usuário não existe']
@@ -64,13 +60,8 @@ class UserController {
 
   async delete(req, res) {
     try {
-      if (!req.params.id) {
-        return res.status(400).json({
-          errors: ['ID não enviado']
-        })
-      }
 
-      const user = await User.findByPk(req.params.id);
+      const user = await User.findByPk(req.userId);
       if (!user) {
         return res.status(400).json({
           errors: ['Usuário não existe']
