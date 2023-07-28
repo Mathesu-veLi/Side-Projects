e939ecd918ca4f29187289123c1bbc81f7c8d757
Matheus Levi <matheuslevit@gmail.com>
2023-07-28 20:50:20 -0300

    Criando função de delete

--- JS/Api Rest/src/controllers/AlunoController.js
+++ JS/Api Rest/src/controllers/AlunoController.js
@@ -9,7 +9,7 @@ class AlunoController {
   async show(req, res) {
     try {
       const { id } = req.params;
-      if(!id) {
+      if (!id) {
         return res.status(400).json({
           errors: ['Faltando ID'],
         });
@@ -17,25 +17,49 @@ class AlunoController {
 
       const aluno = await Aluno.findByPk(id);
 
-      if(!aluno) {
+      if (!aluno) {
         return res.status(400).json({
           errors: ['Aluno não existe'],
         });
       };
 
       return res.json(aluno);
-    } catch(e) {
+    } catch (e) {
       return res.status(400).json({
         errors: e.errors.map(err => err.message)
       });
     };
   };
 
-  async store(req, res) {};
+  async store(req, res) { };
 
-  async update(req, res) {};
+  async update(req, res) { };
 
-  async delete(req, res) {};
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Faltando ID'],
+        });
+      };
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        });
+      };
+
+      await aluno.destroy();
+      return res.json('Aluno deletado com sucesso');
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map(err => err.message)
+      });
+    };
+  };
 };
 
 export default new AlunoController();
