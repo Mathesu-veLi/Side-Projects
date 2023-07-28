a539c8643fc7e2766e9e3e15ddd927da21041761
Matheus Levi <matheuslevit@gmail.com>
2023-07-28 16:01:38 -0300

    Criando rota para token

--- JS/Api Rest/app.js
+++ JS/Api Rest/app.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import homeRoutes from './src/routes/homeRoutes';
 import userRoutes from './src/routes/userRoutes';
+import tokenRoutes from './src/routes/tokenRoutes';
 
 dotenv.config();
 
@@ -22,6 +23,7 @@ class App {
   routes() {
     this.app.use('/', homeRoutes)
     this.app.use('/users/', userRoutes)
+    this.app.use('/tokens', tokenRoutes)
   }
 }
 
