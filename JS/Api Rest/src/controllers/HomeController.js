6659d3269654566389c361174124a79c3191c3af
Matheus Levi <matheuslevit@gmail.com>
2023-07-28 20:16:42 -0300

    Simplificando HomeController

--- JS/Api Rest/src/controllers/HomeController.js
+++ JS/Api Rest/src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Matheus',
-      sobrenome: 'Fernandes',
-      email: 'matheuslevit@gmail.com',
-      idade: 13,
-      peso: 51,
-      altura: 1.54
-    })
-    res.json(novoAluno);
+    res.json('Index');
   };
 };
 
