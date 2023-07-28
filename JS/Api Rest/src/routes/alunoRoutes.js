b25557c6a74224aa4c74b4ce40c2d9a74ba96ee2
Matheus Levi <matheuslevit@gmail.com>
2023-07-28 20:47:37 -0300

    Criando rotas CRUD do aluno

--- JS/Api Rest/src/routes/alunoRoutes.js
+++ JS/Api Rest/src/routes/alunoRoutes.js
@@ -3,6 +3,10 @@ import alunoController from '../controllers/AlunoController';
 
 const router = new Router();
 
-router.get('/', alunoController.index)
+router.get('/', alunoController.index);
+router.get('/', alunoController.show);
+router.post('/:id', alunoController.store);
+router.put('/:id', alunoController.update);
+router.delete('/:id', alunoController.delete);
 
 export default router;
