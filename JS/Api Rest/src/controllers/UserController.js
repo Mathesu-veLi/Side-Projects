906035eab9027159558b0f120b4b69059a9fcd66
Matheus Levi <matheuslevit@gmail.com>
2023-07-28 15:42:25 -0300

    Mostrar usuários

--- JS/Api Rest/src/controllers/UserController.js
+++ JS/Api Rest/src/controllers/UserController.js
@@ -4,13 +4,22 @@ class UserController {
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      res.json(novoUser);
+      return res.json(novoUser);
     } catch (e) {
-      res.status(400).json({
+      return res.status(400).json({
         errors: e.errors.map(erro => erro.message),
       })
     };
   };
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users)
+    } catch(e) {
+      return res.json(null);
+    }
+  }
 };
 
 export default new UserController();
