cb2be692366fad169c6ff696bb51f642edc3ae1b
Matheus Levi <matheuslevit@gmail.com>
2023-07-28 20:52:56 -0300

    Criando função de update

--- JS/Api Rest/src/controllers/AlunoController.js
+++ JS/Api Rest/src/controllers/AlunoController.js
@@ -43,7 +43,31 @@ class AlunoController {
     }
    };
 
-  async update(req, res) { };
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Faltando ID'],
+        });
+      };
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        });
+      };
+
+      const alunoAtualizado = aluno.update(req.body);
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map(err => err.message)
+      });
+    };
+  };
 
   async delete(req, res) {
     try {
