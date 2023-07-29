224241d750d811d9467a3eb91e7f3064641a174e
Matheus Levi <matheuslevit@gmail.com>
2023-07-28 21:18:11 -0300

    Update alunoRoutes.js

--- JS/Api Rest/src/routes/alunoRoutes.js
+++ JS/Api Rest/src/routes/alunoRoutes.js
@@ -5,7 +5,7 @@ const router = new Router();
 
 router.get('/', alunoController.index);
 router.get('/:id', alunoController.show);
-router.post('/:id', alunoController.store);
+router.post('/', alunoController.store);
 router.put('/:id', alunoController.update);
 router.delete('/:id', alunoController.delete);
 
