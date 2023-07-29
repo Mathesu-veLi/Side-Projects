155326d7298dfb65092de90a0ca2f8e738bc3389
Matheus Levi <matheuslevit@gmail.com>
2023-07-28 21:02:43 -0300

    Requesitando id para mostrar aluno específico

--- JS/Api Rest/src/routes/alunoRoutes.js
+++ JS/Api Rest/src/routes/alunoRoutes.js
@@ -4,7 +4,7 @@ import alunoController from '../controllers/AlunoController';
 const router = new Router();
 
 router.get('/', alunoController.index);
-router.get('/', alunoController.show);
+router.get('/:id', alunoController.show);
 router.post('/:id', alunoController.store);
 router.put('/:id', alunoController.update);
 router.delete('/:id', alunoController.delete);
