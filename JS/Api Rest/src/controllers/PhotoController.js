3f7380a759e42128aacbb868cc99ecf71ebef384
Matheus Levi <matheuslevit@gmail.com>
2023-07-31 14:37:19 -0300

    Apresentando erro caso id do aluno não exista

--- JS/Api Rest/src/controllers/PhotoController.js
+++ JS/Api Rest/src/controllers/PhotoController.js
@@ -15,11 +15,20 @@ class PhotoController {
         });
       };
 
-      const { originalname, filename } = req.file;
-      const { aluno_id } = req.body
-      const photo = await Photo.create({ originalname, filename, aluno_id })
+      try {
+        const { originalname, filename } = req.file;
+        const { aluno_id } = req.body
+        const photo = await Photo.create({ originalname, filename, aluno_id })
 
-      res.json(photo);
+        return res.json(photo);
+      } catch (e) {
+        console.log(e)
+        return res.status(400).json({
+          errors: ['ID do aluno não existe']
+        });
+      };
+
+
     });
   };
 };
