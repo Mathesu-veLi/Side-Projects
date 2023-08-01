58ee8d1354cb31a419f220a32e9070cc6afc5a21
Matheus Levi <matheuslevit@gmail.com>
2023-08-01 19:28:18 -0300

    Criando formulário

--- JS/React/lista_de_tarefas/src/components/Main.js
+++ JS/React/lista_de_tarefas/src/components/Main.js
@@ -1,7 +1,32 @@
 import React, { Component } from "react";
 
 export default class Main extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newTask: "",
+    };
+
+    this.validateInput = this.validateInput.bind(this);
+  }
+
+  validateInput(e) {
+    this.setState({
+      newTask: e.target.value,
+    })
+  }
+
   render() {
-    return <h1>Meu component com estado</h1>;
+    return (
+      <div className="main">
+        <h1>Lista de tarefas</h1>
+
+        <form action="#">
+          <input onChange={this.validateInput} type="text" />
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+    );
   }
 }
