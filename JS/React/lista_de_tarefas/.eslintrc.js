5f82429e240829f267f9369b0d1bf53142843058
Matheus Levi <matheuslevit@gmail.com>
2023-08-01 22:36:33 -0300

    Update .eslintrc.js

--- JS/React/lista_de_tarefas/.eslintrc.js
+++ JS/React/lista_de_tarefas/.eslintrc.js
@@ -2,52 +2,30 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    jest: true,
     node: true,
   },
   extends: [
-    'airbnb',
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [
-        '.eslintrc.{js,cjs}',
-      ],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
-  ],
-  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 'latest',
+    requireConfigFile: false,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    'prettier',
-    'react-hooks'
-  ],
-  settings: {
-    react: {
-      version: 'detect',
-    }
-  },
+  plugins: ['react', 'prettier', 'react-hooks'],
   rules: {
-    'react/react-in-jsx-scope': 'off'
+    'prettier/prettier': 2,
+    'react/jsx-filename-extension': 0,
+    'import/prefer-default-export': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'no-undef': 'off',
   },
 };
