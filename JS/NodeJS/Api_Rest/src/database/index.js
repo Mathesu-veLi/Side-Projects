8895c69063d54b3f952001ff8ff647906284fe45
Matheus Levi <matheuslevit@gmail.com>
2023-09-07 18:40:34 -0300

    Adicionando parâmetros

--- JS/NodeJS/Api_Rest/src/database/index.js
+++ JS/NodeJS/Api_Rest/src/database/index.js
@@ -6,7 +6,19 @@ import Photo from "../models/Photo";
 const models = [Aluno, User, Photo];
 const { Sequelize } = require("sequelize-cockroachdb");
 
-const connection = new Sequelize(process.env.DATABASE_URL);
+const connection = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+    'createdAt': 'created_at',
+    'updatedAt': 'updated_at'
+  },
+  dialectOptions: {
+    timezone: 'Etc/GMT-3',
+  },
+  timezone: 'Etc/GMT-3',});
 
 models.forEach(model => model.init(connection));
 models.forEach(model => model.associate && model.associate(connection.models));
