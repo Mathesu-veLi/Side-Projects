4897afd2a869d174ded6e267b43c6ca90031f212
Matheus Levi <matheuslevit@gmail.com>
2023-09-07 10:40:00 -0300

    Correção de timezone

--- JS/NodeJS/Api_Rest/src/config/database.js
+++ JS/NodeJS/Api_Rest/src/config/database.js
@@ -15,7 +15,7 @@ module.exports = {
     'updatedAt': 'updated_at'
   },
   dialectOptions: {
-    timezone: 'America/Bahia',
+    timezone: 'Etc/GMT-3',
   },
-  timezone: 'America/Bahia',
+  timezone: 'Etc/GMT-3',
 };
