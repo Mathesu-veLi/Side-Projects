bfd5dd0fd856b44cb2ed3c532a7be2eee6caf677
Matheus Levi <matheuslevit@gmail.com>
2023-09-07 16:34:13 -0300

    Mudando forma de conexão

--- JS/NodeJS/Api_Rest/src/database/index.js
+++ JS/NodeJS/Api_Rest/src/database/index.js
@@ -1,13 +1,12 @@
-import Sequelize from "sequelize";
 import databaseConfig from '../config/database';
 import Aluno from "../models/Aluno";
 import User from "../models/User";
 import Photo from "../models/Photo";
 
 const models = [Aluno, User, Photo];
+const { Sequelize } = require("sequelize-cockroachdb");
 
-const connection = new Sequelize(databaseConfig);
+const connection = new Sequelize(process.env.DATABASE_URL);
 
 models.forEach(model => model.init(connection));
 models.forEach(model => model.associate && model.associate(connection.models));
-
