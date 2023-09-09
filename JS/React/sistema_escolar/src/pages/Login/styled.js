74123bd406ee993e8ac6980108f198c646f4a3a9
Matheus Levi <matheuslevit@gmail.com>
2023-09-09 12:45:30 -0300

    Retirando coisas que não irei utilizar

--- JS/React/sistema_escolar/src/pages/Login/styled.js
+++ JS/React/sistema_escolar/src/pages/Login/styled.js
@@ -1,9 +1,3 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-  color: ${(props) => (props.isRed ? 'red' : 'blue')}
-`;
-
-export const P = styled.p`
-  color: #000;
-`;
+export const P = styled.p``;
