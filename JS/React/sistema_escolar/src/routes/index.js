3e59582ef324580ad8d0d861b7ab673b5f537c22
Matheus Levi <matheuslevit@gmail.com>
2023-09-09 12:46:10 -0300

    Adicionando novas rotas

--- JS/React/sistema_escolar/src/routes/index.js
+++ JS/React/sistema_escolar/src/routes/index.js
@@ -2,13 +2,23 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 
 import MyRoute from './MyRoute';
+
+import Register from '../pages/Register';
 import Login from '../pages/Login';
+import Aluno from '../pages/Aluno';
+import Alunos from '../pages/Alunos';
+import Photos from '../pages/Photos';
 import Page404 from '../pages/Page404';
 
 export default function Router() {
   return (
     <Switch>
-      <MyRoute exact path="/" component={Login} isClosed />
+      <MyRoute exact path="/" component={Alunos} />
+      <MyRoute exact path="/aluno/:id/edit" component={Aluno} isClosed={true} />
+      <MyRoute exact path="/aluno/" component={Aluno} isClosed={true} />
+      <MyRoute exact path="/fotos/:id" component={Photos} isClosed={true} />
+      <MyRoute exact path="/login" component={Login} />
+      <MyRoute exact path="/register" component={Register} />
       <MyRoute path="*" component={Page404} />
     </Switch>
   );
