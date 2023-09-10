7c03585f796e447bffd11e2aad47b40ed8c41ed0
Matheus Levi <matheuslevit@gmail.com>
2023-09-10 14:10:49 -0300

    Validando dados enviados

--- JS/React/sistema_escolar/src/pages/Login/index.js
+++ JS/React/sistema_escolar/src/pages/Login/index.js
@@ -3,7 +3,31 @@ import React from 'react';
 import { Container } from '../../styles/GlobalStyle';
 import { Form } from './styled';
 
+import { isEmail } from 'validator';
+import { toast } from 'react-toastify';
+
 export default function Login() {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let formErros = false;
+
+    if (!isEmail(email)) {
+      formErros = true;
+      toast.error('Email inválido');
+    }
+
+    if (password.length < 6 || password.length > 50) {
+      // eslint-disable-next-line no-unused-vars
+      formErros = true;
+      toast.error('Senha inválida');
+    }
+
+    if (formErros) return;
+  };
+
   return (
     <Container>
       <h1>Login</h1>
