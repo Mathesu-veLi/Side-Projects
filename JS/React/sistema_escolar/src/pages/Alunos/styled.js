2512fe6731cc832ef815698b7d15758bea9f5b53
Matheus Levi <matheuslevit@gmail.com>
2023-09-09 21:31:45 -0300

    Estilizando tabela de informações e foto

--- JS/React/sistema_escolar/src/pages/Alunos/styled.js
+++ JS/React/sistema_escolar/src/pages/Alunos/styled.js
@@ -1,3 +1,41 @@
 import styled from 'styled-components';
 
-export const P = styled.p``;
+export const AlunoContainer = styled.div`
+  margin-top: 20px;
+
+  table {
+    width: 100%;
+    border-spacing: 10px;
+    border-collapse:collapse
+  }
+
+  th {
+    padding-bottom: 15px;
+  }
+
+  td {
+    padding: 10px 0;
+  }
+
+  table tr:not(.thead) + tr {
+    border-top: 1px solid #afafaf;
+  }
+
+  td * {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  #name {
+    width: 40%;
+  }
+`;
+
+export const ProfilePicture = styled.div`
+  img {
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
+  }
+`;
