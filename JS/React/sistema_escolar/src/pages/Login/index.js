de55dd68bcc722449810cc7cb8387d2e1ebc4030
Matheus Levi <matheuslevit@gmail.com>
2023-09-10 14:10:27 -0300

    Criando formulário de Login e estilizando-o

--- JS/React/sistema_escolar/src/pages/Login/index.js
+++ JS/React/sistema_escolar/src/pages/Login/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
 import { Container } from '../../styles/GlobalStyle';
+import { Form } from './styled';
 
 export default function Login() {
   return (
     <Container>
       <h1>Login</h1>
+
+      <Form onSubmit={handleSubmit}>
+        <label htmlFor="email">
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Seu email"
+          />
+        </label>
+        <label htmlFor="password">
+          Senha:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Seu password"
+          />
+        </label>
+
+        <button type="submit">Acessar</button>
+      </Form>
     </Container>
   );
 }
--- JS/React/sistema_escolar/src/pages/Login/styled.js
+++ JS/React/sistema_escolar/src/pages/Login/styled.js
@@ -1,3 +1,25 @@
 import styled from 'styled-components';
 
-export const P = styled.p``;
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin: 10px 0 10px 0;
+
+  label {
+    display: flex;
+    flex-direction: column;
+    margin: 10px 0 10px 0;
+  }
+
+  input {
+    margin: 5px 0 5px 0;
+    padding: 10px;
+    font-size: 1.03em;
+    border: 1px solid #7e7e7e;
+    border-radius: 5px;
+  }
+
+  input:focus {
+    border: 1px solid #af2f2f;
+  }
+`;
