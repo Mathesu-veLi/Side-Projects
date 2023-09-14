4cba0e377dcd9e860dd41dcdc6a7eec7051864d9
Matheus Levi <matheuslevit@gmail.com>
2023-09-13 21:48:10 -0300

    Criando form para cadastro de alunos

--- JS/React/sistema_escolar/src/pages/Aluno/index.js
+++ JS/React/sistema_escolar/src/pages/Aluno/index.js
@@ -1,19 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { get } from 'lodash';
 import PropTypes from 'prop-types';
 
 import { Container } from '../../styles/GlobalStyle';
+import { Form } from './styled';
 
 export default function Aluno({ match }) {
   const id = get(match, 'params.id', 0);
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+  const [email, setEmail] = useState('');
+  const [age, setAge] = useState('');
+  const [weight, setWeight] = useState('');
+  const [height, setHeight] = useState('');
 
   return (
     <Container>
-      <h1>Aluno</h1>
+      <h1>{id ? 'Editar aluno' : 'Novo aluno'}</h1>
+
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Nome"
+        />
+        <input
+          type="text"
+          value={surname}
+          onChange={(e) => setSurname(e.target.value)}
+          placeholder="Sobrenome"
+        />
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <input
+          type="number"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+          placeholder="Idade"
+        />
+        <input
+          type="text"
+          value={weight}
+          onChange={(e) => setWeight(e.target.value)}
+          placeholder="Peso"
+        />
+        <input
+          type="text"
+          value={height}
+          onChange={(e) => setHeight(e.target.value)}
+          placeholder="Altura"
+        />
+
+        <button type="submit">Enviar</button>
+      </Form>
     </Container>
   );
 }
 
 Aluno.propTypes = {
-  match: PropTypes.shape({}),
+  match: PropTypes.shape({}).isRequired,
 };
