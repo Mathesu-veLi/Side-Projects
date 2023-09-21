3b8727ddfbbab24cb2ee811e09b99fa4175d7672
Matheus Levi <matheuslevit@gmail.com>
2023-09-21 19:07:46 -0300

    Update .eslintrc.js

--- JS/React/sistema_escolar/.eslintrc.js
+++ JS/React/sistema_escolar/.eslintrc.js
@@ -25,7 +25,7 @@ module.exports = {
     },
   },
   rules: {
-    'prettier/prettier': 2,
+    'prettier/prettier': 0,
     'react/jsx-filename-extension': 0,
     'import/prefer-default-export': 0,
     'react-hooks/rules-of-hooks': 'error',
