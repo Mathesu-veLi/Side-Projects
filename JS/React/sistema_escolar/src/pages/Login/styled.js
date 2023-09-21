e2e8b49a6b994b775ae82b82215e543179587532
Matheus Levi <matheuslevit@gmail.com>
2023-09-20 21:04:11 -0300

    Estilizando página de login e registro

--- JS/React/sistema_escolar/src/pages/Login/styled.js
+++ JS/React/sistema_escolar/src/pages/Login/styled.js
@@ -4,6 +4,8 @@ export const Form = styled.form`
   display: flex;
   flex-direction: column;
   margin: 10px 0 10px 0;
+  height: 310px;
+    justify-content: space-around;
 
   label {
     display: flex;
@@ -21,5 +23,29 @@ export const Form = styled.form`
 
   input:focus {
     border: 1px solid #af2f2f;
+  }
+
+  button {
+    margin-top: 20px;
+  }
+`;
+
+export const LoginRegister = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  div {
+    display: flex;
+  justify-content: space-around;
+  }
+
+  section {
+    width: 600px;
+    margin: 30px;
+  }
+
+  section:nth-child(1) form {
+    margin-top: 30px;
   }
 `;
